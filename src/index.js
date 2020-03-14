ac7d547b531ec0d75e99a85a41bf484c0e345a6b
NuclearRedeye <github@mneah.com>
2020-03-14 08:08:32 +0000

    feat: add support for prettier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 module.exports = {
   extends: ['plugin:prettier/recommended'],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        printWidth: 120,
+        useTabs: false,
+        semi: true,
+        bracketSpacing: true,
+        singleQuote: true,
+        trailingComma: 'none',
+        arrowParens: 'always'
+      }
+    ],
     'prefer-const': ['error'] // https://eslint.org/docs/rules/prefer-const
   }
 };
